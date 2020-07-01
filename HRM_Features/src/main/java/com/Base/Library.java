ab1c786481557c35d5cfb0bf041441ddefeccce6
Priya <santhana4ever@gmail.com>
2020-07-01 20:06:07 +0100

    first commit 01/7

---
+++ HRM_Features/src/main/java/com/Base/Library.java
@@ -0,0 +1,52 @@
+package com.Base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Library {
+	
+
+	WebDriver driver;
+
+	public void LaunchApplication(String browser, String url) {
+
+		try {
+			if (browser.equalsIgnoreCase("chrome")){
+				System.setProperty("webdriver.chrome.driver", "C:\\Tools\\Selenium\\New folder\\chromedriver.exe");
+				driver = new ChromeDriver();
+
+			}
+			else if (browser.equalsIgnoreCase("firefox")) {
+
+				System.setProperty("webdriver.gecko.driver", "C:\\Tools\\Selenium\\geckodriver\\geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+
+		}
+
+		catch (WebDriverException e) {
+			System.out.println("Browser not found");
+
+
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(url);
+
+	}
+	
+	public void close() {
+		
+		driver.quit();
+	}
+
+
+}
+
+
+
